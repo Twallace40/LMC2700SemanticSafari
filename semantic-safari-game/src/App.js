631547c900f3d4e8cbe20840e51bf7ef400bfc19
Twallace40 <twallace40@gatech.edu>
2023-11-27 09:37:44 -0500

    Added OpenAI API Skeleton

--- semantic-safari-game/src/App.js
+++ semantic-safari-game/src/App.js
@@ -1,11 +1,36 @@
-import logo from "./logo.svg";
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import OPENAI_API_KEY from './config/openai.js';
 function App() {
-	return (
-		<div className="App">
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
-	);
+  const [response, setResponse] = useState('');
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          'https://api.openai.com/v1/engines/davinci-codex/completions',
+          {
+            prompt: 'Once upon a time',
+            max_tokens: 100,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${OPENAI_API_KEY}`,
+            },
+          }
+        );
+        setResponse(response.data.choices[0].text);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>OpenAI React Demo</h1>
+      <p>{response}</p>
+    </div>
+  );
 }
-
 export default App;
