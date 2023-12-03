500b4eadbf97babb48a80ab141c91a2b0777f09e
rkaimal30 <152322878+rkaimal30@users.noreply.github.com>
2023-12-02 20:31:59 -0500

    Update server.js randomize options

--- semantic-safari-game/backend/server.js
+++ semantic-safari-game/backend/server.js
@@ -10,12 +10,12 @@ app.use(cors()); // Enable CORS
 app.use(bodyParser.json());
 
 // Initialize OpenAI with the API key from environment variables
-const openai = new OpenAI({ apiKey: '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' });
+const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_API_KEY }));
 
 app.post('/api/generate-questions', async (req, res) => {
   try {
-    // New prompt structure asking for a question followed by multiple choices
-    const prompt = 'Generate a multiple-choice question with one correct answer and three plausible but incorrect answers. List the answers as options A, B, C, and D.';
+    // New prompt structure asking for a question with multiple choices
+    const prompt = 'Generate a multiple-choice question and provide four options.';
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: prompt,
@@ -26,12 +26,27 @@ app.post('/api/generate-questions', async (req, res) => {
     // Extract the generated text
     const generatedText = completion.data.choices[0].text.trim();
     
-    // TODO: Parse the generatedText to separate the question and the answer choices.
-    // This parsing logic will depend on the structure of the generatedText.
-    // You may need to manually identify the correct and incorrect answers based on the structure.
+    // Here you would parse the generatedText to separate the question from the options
+    // The parsing logic will depend on the structure of the generatedText.
+    // Since the AI does not know which is the correct answer, you would have to designate one.
+    // For example, if the output is structured with the question followed by four options, you might do:
 
-    // For now, return the generatedText to the client for demonstration
-    res.json({ generatedText: generatedText });
+    // Split the text by newline to separate the question and options
+    const lines = generatedText.split('\n');
+    const question = lines[0];
+    const options = lines.slice(1); // This assumes the options follow the question and are on separate lines
+
+    // Randomly select a correct answer
+    const correctIndex = Math.floor(Math.random() * options.length);
+    const correctAnswer = options[correctIndex];
+
+    const quizQuestion = {
+      definition: question,
+      options: options,
+      correctAnswer: correctAnswer,
+    };
+
+    res.json({ question: quizQuestion });
   } catch (error) {
     console.error('Error generating question:', error);
     res.status(500).json({ error: 'Internal Server Error' });
