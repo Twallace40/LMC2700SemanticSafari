582c123ad247990ae3e2aa7d3b8438523279d3b5
rkaimal30 <152322878+rkaimal30@users.noreply.github.com>
2023-12-02 21:51:42 -0500

    Update server.js revert

--- semantic-safari-game/backend/server.js
+++ semantic-safari-game/backend/server.js
@@ -1,27 +1,46 @@
-const fs = require('fs').promises;
-const { Configuration, OpenAIApi } = require("openai");
+const express = require('express');
+const bodyParser = require('body-parser');
+const OpenAI = require('openai');
+const cors = require('cors');
 
-const configuration = new Configuration({
-  apiKey: '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',
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(cors()); // Enable CORS
+
+app.use(bodyParser.json());
+
+const openai = new OpenAI({ apiKey: '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' });
+
+app.post('/api/generate-questions', async (req, res) => {
+
+  try {
+    const prompt = 'Generate a quiz question: ';
+    const completion = await openai.completions.create({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 100,
+      });
+
+    // Extract the generated question from the API response
+    const generatedQuestion = completion.choices[0].text.trim();
+
+    // For simplicity, use a static set of answer choices
+    const options = ['Choice A', 'Choice B', 'Choice C', 'Choice D'];
+    const correctAnswer = options[Math.floor(Math.random() * options.length)];
+
+    const quizQuestion = {
+      definition: generatedQuestion,
+      options: options,
+      correctAnswer: correctAnswer,
+    };
+
+    res.json({ question: quizQuestion });
+  } catch (error) {
+    console.error('Error generating question:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
-const openai = newOpenAIApi(configuration);
 
-const shape = [{
-   "question": "What is the process by which green plants and some other organisms use sunlight to synthesize foods from carbon dioxide and water, involving the green pigment chlorophyll and generating oxygen as a by-product?",
-    "answer": "Photosynthesis",
-    "wrongAnswers": ["Respiration", "Fermentation", "Condensation"]
-}]
-  
-(async function run() {
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo", 
-    messages: [{
-      role: "user", 
-      content: 'Generate 10 questions in the format of the question being the definition of a word and the answer being the word. include wrong answers. format the response as JSON in the shape of: ${JSON.stringify(shape)}'
-    }],
-  });
-  console.log(completion.data.choices[0].message);
-  const questions = JSON.parse(completion.data.choices[0].message.content;
-  console.log('questions'), questions
-  await fs.writeFile('./src/questions.json'), JSON.stringify(questions, null, 2))
-})();
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
